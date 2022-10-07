64079ff3e6956af3c5ca4a8c0dce6c2c469edddf
t19408ss <t19408ss@sfc.keio.ac.jp>
2022-10-07 16:02:04 +0900

    20221007

---
+++ 20221007/as2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct list {
+  struct list *next;
+  int data;
+};
+
+int main() {
+  struct list *c, *p;
+  int i;
+  c = malloc(sizeof(struct list));
+  p = c;
+  for (i = 0; i < 100; i++) {
+    c->data = i + 1;
+    c->next = malloc(sizeof(struct list));
+    c = c->next;
+  }
+  for (i = 0; i < 100; i++) {
+    printf("%d\n", p->data);
+    p = p->next;
+  }
+  exit(0);
+}
